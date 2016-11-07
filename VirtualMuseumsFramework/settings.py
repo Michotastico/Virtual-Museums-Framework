f38cf5fdd1664ee4b1ed47f7215fdc94b7fc9bf3
Michel Llorens <mllorens@dcc.uchile.cl>
2016-11-07 12:36:11 -0300

    Rename to project to more formal name

--- VirtualMuseumsFramework/settings.py
+++ VirtualMuseumsFramework/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoTest.urls'
+ROOT_URLCONF = 'VirtualMuseumsFramework.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoTest.wsgi.application'
+WSGI_APPLICATION = 'VirtualMuseumsFramework.wsgi.application'
 
 
 # Database
