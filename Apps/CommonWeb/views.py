feed6888b18c795c7fd73c2ae7776f9617dedcab
Michel Llorens <mllorens@dcc.uchile.cl>
2017-02-06 15:12:18 -0300

    Add basic login

--- Apps/CommonWeb/views.py
+++ Apps/CommonWeb/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import TemplateView
@@ -9,3 +10,32 @@ class IndexView(TemplateView):
 
     def get(self, request, *a, **ka):
         return render(request, self.template_name)
+
+
+class LoginView(TemplateView):
+    template_name = 'common-web/login.html'
+
+    def get(self, request, *a, **ka):
+        if request.user.is_authenticated():
+            return redirect('/curator')
+        return render(request, self.template_name)
+
+    def post(self, request):
+        user = authenticate(
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+
+        if user is None:
+            return render(request, self.template_name, {'message': 'Incorrect credentials'})
+
+        else:
+            login(request, user)
+            return redirect('/curator')
+
+
+class LogoutView(TemplateView):
+
+    def get(self, request, *a, **ka):
+        logout(request)
+        return redirect('/')
