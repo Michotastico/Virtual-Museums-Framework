3a307b64f20833e3cdc30ec6419b17796ea29d81
Michel Llorens <mllorens@dcc.uchile.cl>
2017-03-03 18:50:46 -0300

    Finished resources templates refactor to allow multiple types

--- Apps/Curator/views.py
+++ Apps/Curator/views.py
@@ -1,3 +1,4 @@
+import copy
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -48,10 +49,15 @@ class RoomsView(TemplateView):
 
 class ResourcesView(TemplateView):
     template_name = 'curator/resources/resources.html'
+    selector = {'header': {'display': 'Objects, Music, Images, etc:',
+                           'selected': 'selected'},
+                'options': [{'value': 'music', 'display': 'Music', 'selected': ''},
+                            {'value': 'images', 'display': 'Images', 'selected': ''},
+                            {'value': 'objects', 'display': 'Objects', 'selected': ''}]}
 
     @method_decorator(login_required(login_url='/auth/login'))
     def get(self, request, *a, **ka):
-        return render(request, self.template_name)
+        return render(request, self.template_name, self.selector)
 
     @method_decorator(login_required(login_url='/auth/login'))
     def post(self, request, *a, **ka):
@@ -59,13 +65,25 @@ class ResourcesView(TemplateView):
         specific_resource = request.POST.get('resource', None)
         new = request.POST.get('new-resource', None)
 
+        specific_template = self.template_name
+        specific_selector = copy.deepcopy(self.selector)
+
         if specific_resource:
-            # TODO redirect to specific resource webpage
-            print specific_resource
+            specific_selector['header']['selected'] = ''
+            if specific_resource == 'music':
+                specific_template = 'curator/resources/resources-music.html'
+                specific_selector['options'][0]['selected'] = 'selected'
+            elif specific_resource == 'images':
+                specific_template = 'curator/resources/resources-images.html'
+                specific_selector['options'][1]['selected'] = 'selected'
+            elif specific_resource == 'objects':
+                specific_template = 'curator/resources/resources-objects.html'
+                specific_selector['options'][2]['selected'] = 'selected'
+
         if new in ['1']:
             # TODO redirect to upload page
             print new
-        return render(request, self.template_name)
+        return render(request, specific_template, specific_selector)
 
 
 class SchedulingView(TemplateView):
