c90ee0d6b96a9ac0d518daaf535b22c9fd81ff56
Michel Llorens <mllorens@dcc.uchile.cl>
2017-03-21 14:33:35 -0300

    Room JSON template example

--- Apps/Curator/models/rooms.py
+++ Apps/Curator/models/rooms.py
@@ -24,3 +24,17 @@ class Room(models.Model):
 
     def set_resources(self, resource_as_dict):
         self.resources = json.dumps(resource_as_dict)
+
+
+'''
+Resource JSON structure example:
+{
+    'width': number,
+    'height': number,
+    'lookup_image': url,
+    'ground_layer': [{'type': ground/grass/stone/etc, 'x': number, 'y':number}],
+    'object_layer': [{'type': image/model, 'id':number,
+                    'orientation': N/S/W/E, 'x': number, 'y':number}],
+    'vegetation_layer': [{'type': bush/tree/etc, 'x': number, 'y':number}]
+}
+'''
